7fc391165d7a2c188de5f92fdf4cce0c77ba2768
Ceddy Muhoza <ceddymuhoza@gmail.com>
2021-01-25 06:19:29 -0500

    init Sofa connection

---
+++ src/connection.ts
@@ -0,0 +1,70 @@
+/* eslint-disable @typescript-eslint/ban-ts-comment */
+import * as couchbase from 'couchbase';
+
+interface SofaArgs {
+    connectionString: string;
+    bucketName: string;
+    username: string;
+    password: string;
+}
+
+class SofaConnection implements SofaArgs {
+    private static _instance: SofaConnection;
+
+    bucket: Bucket = null;
+    cluster: Cluster = null;
+
+    // Args
+    connectionString: string;
+    bucketName: string;
+    username: string;
+    password: string;
+
+    public static get Instance(): SofaConnection {
+        return this._instance || (this._instance = new this());
+    }
+
+    private constructor() {}
+
+    /**
+     * start
+     */
+    public init(args: SofaArgs): {bucket: Bucket; cluster: Cluster} {
+        const {connectionString, password, username, bucketName = 'default'} = args;
+
+        this.connectionString = connectionString;
+        this.bucketName = bucketName;
+        this.username = username;
+        this.password = password;
+
+        // @ts-ignore
+        this.cluster = new couchbase.Cluster(connectionString, {
+            username,
+            password,
+        });
+        this.bucket = this.cluster.bucket(bucketName);
+
+        return this;
+    }
+
+    /**
+     * getCollection
+     */
+    public getCollection(collectionName?: string): Collection {
+        return collectionName
+            ? this.bucket.collection(collectionName)
+            : this.bucket.defaultCollection();
+    }
+
+    /**
+     * start
+     */
+    public async start(): Promise<boolean> {
+        // Create a N1QL Primary Index (but ignore if it exists)
+        try {
+            await this.cluster.queryIndexes().createPrimaryIndex(bucketName, {ignoreExists: true});
+        } catch (e) {}
+    }
+}
+
+export default SofaConnection;
