4d1a319bfbe7892d4973fe6e8bb02b7fe2185ab6
Ceddy Muhoza <ceddymuhoza@gmail.com>
2021-01-25 07:15:36 -0500

    add tests

---
+++ src/index.test.ts
@@ -0,0 +1,20 @@
+import 'mocha';
+import { expect } from 'chai';
+
+describe('Sofa', () => {
+    it('should insert into couchbase', () => {
+
+    })
+
+    it('should get into couchbase', () => {
+        
+    })
+
+    it('should delete into couchbase', () => {
+        
+    })
+
+    it('should update into couchbase', () => {
+        
+    })
+})
